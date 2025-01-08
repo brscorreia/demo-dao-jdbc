8245f02ce92f95714e648a55c7457d288a89ecc6
unknown <bcorreia@gmail.com>
2025-01-08 10:38:33 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		//Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21 , "Bob", "bob@mail", new Date(), 3000.0, obj);
+		//Seller seller = new Seller(21 , "Bob", "bob@mail", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(1);
 		
 		System.out.println(seller);
 	}
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
-import model.dao.impl.SellerDaoJdbc;
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJdbc();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
